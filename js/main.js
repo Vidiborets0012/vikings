177ba5dd3e5174c723549910e6d61767df6dcc78
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-03 14:46:27 +0200

    link heroes-slider-img and heroes-slider-text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 Fancybox.bind('[data-fancybox="slogan-video"]', {
        
-      }); 
+}); 
+      
+$(function () {
+
+    $('.heroes-slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,        
+        asNavFor: '.heroes-slider-text'
+    });
+
+    $('.heroes-slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.heroes-slider-img'
+    });
+
+});
