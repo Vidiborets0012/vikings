41805208d722dbeed612c8635f1bf6f35169beee
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-01-03 15:24:23 +0200

    add styles for the heroes section and style the sliders

--- js/main.js
+++ js/main.js
@@ -7,13 +7,16 @@ $(function () {
     $('.heroes-slider-img').slick({
         slidesToShow: 1,
         slidesToScroll: 1,        
-        asNavFor: '.heroes-slider-text'
+        asNavFor: '.heroes-slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/heroes-arrow-left.png" alt="prev" /></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/heroes-arrow-right.png" alt="next" /></button>'
     });
 
     $('.heroes-slider-text').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
+        arrows: false,
         asNavFor: '.heroes-slider-img'
     });
 
